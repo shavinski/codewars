6d45ff904ac55c19cf299f2e81ac3807fc03afc4
Jakob <shavinski.jakob@gmail.com>
2024-02-05 13:58:02 -0800

    Finish string to array

--- 8kyu/digitize.py
+++ 8kyu/digitize.py
@@ -1,4 +1,6 @@
 def digitize(n):
+    # could do this:
+    # return [int(num) for num in str(n)[::-1]]
     result = [int(num) for num in str(n)]
     result.reverse()
     return result
--- 8kyu/smallestNum.py
+++ 8kyu/smallestNum.py
@@ -1,9 +1,11 @@
 def find_smallest_int(arr):
     # Code here
+    # could do this
+    # return min(arr)
     smallestNum = arr[0]
-    
+
     for num in arr:
         if num < smallestNum:
             smallestNum = num
-            
+
     return smallestNum
